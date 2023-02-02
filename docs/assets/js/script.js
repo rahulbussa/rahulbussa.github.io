84375cf5f991e922eea4305174ab5299b4402365
Rahul Bussa <rahulbussa@gmail.com>
2023-02-02 16:20:11 +0530

    animation

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -101,7 +101,7 @@ const sr = ScrollReveal({
     reset: true
 });
 
-sr.reveal(`.home__data, .home__img,
+sr.reveal(`.home__data,.home__social, .home__img,
             .about__data, .about__img,
             .services__content, .menu__content,
             .app__data, .app__img,
